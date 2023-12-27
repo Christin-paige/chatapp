4d06c12c1df13a2747663f63deb2e4886861c4e1
Christin-paige <christinpmartin@gmail.com>
2023-12-27 08:54:14 -0500

    adding offline changes

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 //initializing Firestore connection
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+//disable reconnection attempts by firestore db and add to useEffect
+import { getFirestore, disableNetwork, enableNetwork  } from "firebase/firestore";
 
 // import the screens
 import Screen1 from './components/Screen1';
@@ -10,14 +11,32 @@ import Screen2 from './components/Screen2';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+//installed netInfo through the CLI and now importing it
+import { useNetInfo }from '@react-native-community/netinfo';
+
+import { useEffect } from 'react';
+import { LogBox, Alert } from 'react-native';
 
 
 //initialize cloud firestore and ger a reference to the service
-
-
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+   //defines network connectivity status
+   const connectionStatus = useNetInfo();
+  
+
+ 
+  //display an alert if status is lost
+  useEffect(() => {
+    if (connectionStatus.isConnected === false) {
+      Alert.alert("Connection Lost!");
+      disableNetwork(db);
+    } else if (connectionStatus.isConnected === true) {
+      enableNetwork(db);
+    }
+  }, [connectionStatus.isConnected]);
+
   const firebaseConfig = {
     apiKey: "AIzaSyDXDi-F-f9uuj1P3tN0LiMceMg9QpyxVPY",
     authDomain: "chatapp-f2091.firebaseapp.com",
@@ -26,8 +45,9 @@ const App = () => {
     messagingSenderId: "1098929749207",
     appId: "1:1098929749207:web:b8bcac04e479fcae290492"
   };
-
+//initialize firebase
   const app = initializeApp(firebaseConfig);
+  //initialize cloud firestore and get reference to service
   const db = getFirestore(app);
 
  return (
@@ -41,10 +61,8 @@ const App = () => {
          />
        <Stack.Screen
        name="Chat"
-       
        >
-        {props => <Screen2 db={db} {...props}/>}
-      
+        {props => <Screen2 isConnected={connectionStatus.isConnected} db={db} {...props}/>}
        </Stack.Screen>
      </Stack.Navigator>
    </NavigationContainer>
--- components/Screen2.js
+++ components/Screen2.js
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react';
-import { GiftedChat, Bubble } from "react-native-gifted-chat";
+import { GiftedChat, Bubble, InputToolbar } from "react-native-gifted-chat";
 import { StyleSheet, View, KeyboardAvoidingView, Platform } from 'react-native';
 import { collection, addDoc, onSnapshot, query, orderBy } from 'firebase/firestore';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const Screen2 = ({ route, navigation, db}) => {
+const Screen2 = ({ route, navigation, db, isConnected}) => {
   const { name } = route.params;
   const { userID } = route.params;
   const { backgroundColor } = route.params;
@@ -14,24 +15,42 @@ const Screen2 = ({ route, navigation, db}) => {
    newMessages[0])
   }
 
+  let unsubMessages;
+
    useEffect(() => {
-       
+    
+       if (isConnected === true){
        const q = query(collection(db,"messages"), orderBy("createdAt",
        "desc"))
-       const unsubMessages = onSnapshot(q, (docs) => {
+       unsubMessages = onSnapshot(q, async (docs) => {
         let newMessages = [];
         docs.forEach(doc => {
           newMessages.push({ 
             id: doc.id, ...doc.data(), 
             createdAt: doc.data().createdAt.toDate() })
         });
+        cacheMessages(newMessages);
         setMessages(newMessages);
        });
+      }else loadCachedMessages();
+
        //clean up code
        return () => {
        if (unsubMessages) unsubMessages();
        }
-     }, []);
+     }, [isConnected]);
+//adding async storage
+const loadCachedMessages = async () => {
+  const cachedMessages = await AsyncStorage.getItem("messages") || [];
+  setMessages(JSON.parse(cachedMessages));
+}
+     const cacheMessages = async (messagesToCache) => {
+      try {
+        await AsyncStorage.setItem("messages", JSON.stringify(messagesToCache));
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
 
      useEffect(() => {
       navigation.setOptions({ title: name })
@@ -51,6 +70,10 @@ const Screen2 = ({ route, navigation, db}) => {
       }}
     />
   }
+  const renderInputToolbar = (props) => {
+    if (isConnected) return <InputToolbar {...props} />;
+    else return null;
+   }
 
 return (
  <View style={[styles.container, {backgroundColor:backgroundColor}]}>
@@ -58,6 +81,7 @@ return (
      name={name}
      messages={messages}
      renderBubble={renderBubble}
+     renderInputToolbar={renderInputToolbar}
      onSend={messages => onSend(messages)}
      user={{
       _id: userID,
